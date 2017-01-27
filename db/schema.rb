41e457c562be24ce3c944f6e3fbd7b1e6068cc5f
David Allen <dma@gnu.org>
2017-01-26 20:47:15 -0600

    Added CKEditor as the article suite editor

--- db/schema.rb
+++ db/schema.rb
@@ -27,6 +27,22 @@ ActiveRecord::Schema.define(version: 20170126212341) do
     t.index ["user_id"], name: "index_articles_on_user_id"
   end
 
+  create_table "ckeditor_assets", force: :cascade do |t|
+    t.string   "data_file_name",               null: false
+    t.string   "data_content_type"
+    t.integer  "data_file_size"
+    t.string   "data_fingerprint"
+    t.integer  "assetable_id"
+    t.string   "assetable_type",    limit: 30
+    t.string   "type",              limit: 30
+    t.integer  "width"
+    t.integer  "height"
+    t.datetime "created_at",                   null: false
+    t.datetime "updated_at",                   null: false
+    t.index ["assetable_type", "assetable_id"], name: "idx_ckeditor_assetable"
+    t.index ["assetable_type", "type", "assetable_id"], name: "idx_ckeditor_assetable_type"
+  end
+
   create_table "taggings", force: :cascade do |t|
     t.integer  "tag_id"
     t.integer  "article_id"
